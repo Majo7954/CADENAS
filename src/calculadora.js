0152fd2f25871d3d5632139ef456da15973e0e2b
Maria Jose Murillo <murillomendoza.mariajose@gmail.com>
2024-05-14 05:26:01 -0400

    devuelve el número cuando la cadena tiene un solo elemento

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,4 +1,9 @@
 function calcularCadena(cadena) {
+  if (cadena === "") {
     return 0;
+  }
+
+  return parseInt(cadena);
 }
+
 export default calcularCadena;
--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -4,4 +4,8 @@ describe("Calcular Cadena", () => {
   it("devuelve 0 si la cadena está vacía", () => {
     expect(calcularCadena("")).toEqual(0);
   });
+
+  it("devuelve el número cuando la cadena tiene un solo elemento", () => {
+    expect(calcularCadena("5")).toEqual(5);
+  });
 });
